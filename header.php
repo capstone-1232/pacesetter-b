750f45e76b727a3ffe54b6ae189680924760295b
Trezzon <tcabillo1@nait.ca>
2024-03-07 14:51:23 -0700

    Added keywords, svg titles, made navbar hours of operation dynamic

--- header.php
+++ header.php
@@ -12,10 +12,16 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<title><?php bloginfo('name'); wp_title();?></title>
-	<meta name="description" content="<?php bloginfo('description');?>">
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<title>
+		<?php bloginfo('name');
+		wp_title(); ?>
+	</title>
+	<meta name="description" content="<?php bloginfo('description'); ?>">
+	<meta name="keywords"
+		content="Ski, Snowboard, Edmonton, Sports, Store, Online, Alberta, Services, Rentals, Events, Experienced, Local, West">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -26,47 +32,66 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-<div style="background-color: #013562;">
-		<div>
-			<p>Mon-Fri: 9:30 am to 5:30pm</p>
-			<p>(780) 483-2005</p>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<div style="background-color: #013562;">
+			<div>
+				<p><?php echo get_post_meta($post_id, 'hours_mon-fri', true)?> </p>
+				<p><?php echo get_post_meta($post_id, 'phone_number', true)?> </p>
+				
+			</div>
+			<div>
+				<?php get_search_form(); ?>
+				<a href="<?php echo wc_get_cart_url(); ?>">
+					<svg width="33px" height="33px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<title>This is an image of a cart</title>
+						<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+						<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+						<g id="SVGRepo_iconCarrier">
+							<path
+								d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
+								stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+						</g>
+					</svg>
+				</a>
+			</div>
 		</div>
-		<div>
-		<?php get_search_form(); ?>
-			<a href="<?php echo wc_get_cart_url();?>">
-			<svg width="33px" height="33px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-			</a>
-		</div>
-	</div>
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pacesetter' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-?>
-		</div><!-- .site-branding -->
+		<a class="skip-link screen-reader-text" href="#primary">
+			<?php esc_html_e('Skip to content', 'pacesetter'); ?>
+		</a>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pacesetter' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
+				<?php
+				the_custom_logo();
+				if (is_front_page() && is_home()):
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+							<?php bloginfo('name'); ?>
+						</a></h1>
+					<?php
+				else:
+					?>
+					<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+							<?php bloginfo('name'); ?>
+						</a></p>
+					<?php
+				endif;
+				?>
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" aria-label="Main Menu toggle">
+					<?php esc_html_e('Primary Menu', 'pacesetter'); ?>
+				</button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id' => 'primary-menu',
+					)
+				);
+				?>
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
