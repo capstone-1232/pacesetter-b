d7434edc714e6943b713883bbbb0af30e4e69037
KirstynH <kirstynhowell01@gmail.com>
2024-03-13 15:38:48 -0600

    faq page basic structure for sections and h3s as well as the link for the contact page

--- page-faq.php
+++ page-faq.php
@@ -14,4 +14,45 @@
 get_header();
 ?>
 
-<h2>FAQ</h2>
+<section>
+    <h2>Frequently Asked Questions</h2>
+    <p>Our FAQ page provides answers to common inquiries regarding ski equipment, maintenance, events, and more, ensuring you have a smooth and enjoyable skiing experience.</p>
+</section>
+<section>
+    <h3>Most Common Questions</h3>
+    <div>
+
+    </div>
+</section>
+<!-- sure if the FAQ sections will be spaced together or just a post type, added just to have on page for now -->
+<div>
+    <section>
+        <h3>Skis and Poles</h3>
+    
+    </section>
+    <section>
+        <h3>Snowboards</h3>
+    
+    </section>
+    <section>
+        <h3>Care and Upkeep</h3>
+    
+    </section>
+    <section>
+        <h3>Helmets</h3>
+    
+    </section>
+    <section>
+        <h3>Shop Events/ RSVP</h3>
+    
+    </section>
+    <section>
+        <h3>Returns</h3>
+    
+    </section>
+</div>
+<section>
+    <h3>Still Have Questions?</h3>
+    <p>Phone, email, fill out our contact form or visit us in person on 167th St.</p>
+    <a href="page-contact-us.php">Contact Page</a>
+</section>
