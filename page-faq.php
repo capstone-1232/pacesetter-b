bd97a41e2966d8ee1f3e77a8cfb05a689794b4c1
[neitlaus] <112661025+neitlaus@users.noreply.github.com>
2024-03-13 15:51:39 -0600

    created faq layout

--- page-faq.php
+++ page-faq.php
@@ -13,5 +13,116 @@
  */
 get_header();
 ?>
+<section>
+    <h2>Frequently Asked Questions</h2>
+    <p>Our FAQ page provides answers to common inquiries regarding ski equipment, maintenance, events, and more, ensuring you have a smooth and enjoyable skiing and snowboarding experience.</p>
+</section>
+<section>
+    <h3>Skis and Poles</h3>
+    <?php $args=array(
+                'post_type' => 'ski_faq',
+                'post_status'=>'publish'
+            );
+            $loop=new WP_Query($args);
+            while ($loop->have_posts()){
+                $loop-> the_post();
+                ?>
+                <div>
+                    <h4><?php echo esc_html(get_field('title'));?></h4>
+                    <p><?php esc_html(get_field('faq_answer'));?></p>
+                </div>
 
-<h2>FAQ</h2>
+<?php }?>
+</section>
+<section>
+    <h3>Snowboards</h3>
+    <?php $args=array(
+                'post_type' => 'snowboard_faq',
+                'post_status'=>'publish'
+            );
+            $loop=new WP_Query($args);
+            while ($loop->have_posts()){
+                $loop-> the_post();
+                ?>
+                <div>
+                    <h4><?php echo esc_html(get_field('title'));?></h4>
+                    <p><?php esc_html(get_field('faq_answer'));?></p>
+                </div>
+
+<?php }?>
+</section>
+
+<section>
+    <h3>Care and Upkeep</h3>
+    <?php $args=array(
+                'post_type' => 'care_faq',
+                'post_status'=>'publish'
+            );
+            $loop=new WP_Query($args);
+            while ($loop->have_posts()){
+                $loop-> the_post();
+                ?>
+                <div>
+                    <h4><?php echo esc_html(get_field('title'));?></h4>
+                    <p><?php esc_html(get_field('faq_answer'));?></p>
+                </div>
+
+<?php }?>
+</section>
+<section>
+    <h3>Helmets</h3>
+    <?php $args=array(
+                'post_type' => 'helmet_faq',
+                'post_status'=>'publish'
+            );
+            $loop=new WP_Query($args);
+            while ($loop->have_posts()){
+                $loop-> the_post();
+                ?>
+                <div>
+                    <h4><?php echo esc_html(get_field('title'));?></h4>
+                    <p><?php esc_html(get_field('faq_answer'));?></p>
+                </div>
+
+<?php }?>
+</section>
+<section>
+    <h3>Shop Events/RSVP</h3>
+    <?php $args=array(
+                'post_type' => 'event_faq',
+                'post_status'=>'publish'
+            );
+            $loop=new WP_Query($args);
+            while ($loop->have_posts()){
+                $loop-> the_post();
+                ?>
+                <div>
+                    <h4><?php echo esc_html(get_field('title'));?></h4>
+                    <p><?php esc_html(get_field('faq_answer'));?></p>
+                </div>
+
+<?php }?>
+</section>
+<section>
+    <h3>Returns</h3>
+    <?php $args=array(
+                'post_type' => 'return_faq',
+                'post_status'=>'publish'
+            );
+            $loop=new WP_Query($args);
+            while ($loop->have_posts()){
+                $loop-> the_post();
+                ?>
+                <div>
+                    <h4><?php echo esc_html(get_field('title'));?></h4>
+                    <p><?php esc_html(get_field('faq_answer'));?></p>
+                </div>
+
+<?php }?>
+</section>
+
+<section>
+    <h3>Still have Questions?</h3>
+    <p>Phone, email, fill out our contact form or visit us in person on 167th St.</p>
+    <a href="<?php echo get_template_directory_uri()?>/page-contact-us.php"></a>
+</section>
