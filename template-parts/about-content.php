168db43b4d45744ed0b88987b16eb75c89df24d0
KirstynH <kirstynhowell01@gmail.com>
2024-03-14 14:26:26 -0600

    change main scss to _main scss and added utils and reset files

--- template-parts/about-content.php
+++ template-parts/about-content.php
@@ -1,6 +1,6 @@
-<div>
+<section class="front-page-about">
     <h2>About Pacesetter</h2>
     <p>Pacesetter Ski and Snowboard Store is your premier destination for all things winter sports. With a curated selection of top-quality equipment and gear, combined with expert knowledge and personalized customer service, we strive to elevate your skiing and snowboarding experience to new heights. Whether you're a seasoned pro or hitting the slopes for the first time, trust Pacesetter to outfit you for adventure in style and comfort.</p>
-    <a href="">Read More</a>
-    <a href="">Contact Us</a>
-</div>
+    <a class="" href="#">Read More</a>
+    <a class="" href="#">Contact Us</a>
+</section>
