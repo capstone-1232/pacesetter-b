3ba8bc35b7d2dbb656ec5b6225c0dfd3225a6b2a
Trezzon <tcabillo1@nait.ca>
2024-03-14 11:16:47 -0600

    echo ifx

--- template-parts/about-content.php
+++ template-parts/about-content.php
@@ -5,7 +5,7 @@
         we strive to elevate your skiing and snowboarding experience to new heights. Whether you're a seasoned pro or
         hitting the slopes for the first time, trust Pacesetter to outfit you for adventure in style and comfort.</p>
     <div class="links">
-        echo "<a href="/about-us">Read More</a>";
-        echo "<a href="/contact-us">Contact Us</a>";
+        <a href="/about-us">Read More</a>;
+        <a href="/contact-us">Contact Us</a>;
     </div>
 </section>
