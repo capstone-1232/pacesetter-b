a4702472016a5e7f80ab515baf4bb0704105a80d
Trezzon <tcabillo1@nait.ca>
2024-03-14 09:47:06 -0600

    more semantic markup, added classes, made links dynamic to only show if page exists, made links link to pages

--- template-parts/about-content.php
+++ template-parts/about-content.php
@@ -1,6 +1,17 @@
-<div>
+<section class="about_section">
     <h2>About Pacesetter</h2>
-    <p>Pacesetter Ski and Snowboard Store is your premier destination for all things winter sports. With a curated selection of top-quality equipment and gear, combined with expert knowledge and personalized customer service, we strive to elevate your skiing and snowboarding experience to new heights. Whether you're a seasoned pro or hitting the slopes for the first time, trust Pacesetter to outfit you for adventure in style and comfort.</p>
-    <a href="">Read More</a>
-    <a href="">Contact Us</a>
-</div>
+    <p>Pacesetter Ski and Snowboard Store is your premier destination for all things winter sports. With a curated
+        selection of top-quality equipment and gear, combined with expert knowledge and personalized customer service,
+        we strive to elevate your skiing and snowboarding experience to new heights. Whether you're a seasoned pro or
+        hitting the slopes for the first time, trust Pacesetter to outfit you for adventure in style and comfort.</p>
+    <div class="links">
+        <?php
+        if (get_page_by_path("about-us", OBJECT)) {
+            echo "<a href=\"/about-us\">Read More</a>";
+        }
+        if (get_page_by_path("contact-us", OBJECT)) {
+            echo "<a href=\"/contact-us\">Contact Us</a>";
+        }
+        ?>
+    </div>
+</section>
