360ca00e5a4024b7cc8ac8282c275a69532086f0
KirstynH <kirstynhowell01@gmail.com>
2024-03-14 15:48:30 -0600

    html structure and img for events on landing page

--- template-parts/hosted-events.php
+++ template-parts/hosted-events.php
@@ -1,10 +1,10 @@
-<div>
+<section class="landing-page-events">
     <div>
-        <img src="" alt="">
+        <img src="landingpage-events.jpg" alt="People having a good time at an event, seated around three people standing and talking.">
     </div>
     <div>
         <h3>Store Hosted Events</h3>
         <p>Discover the latest events at our ski and snowboard store, guaranteed to ignite your winter excitement!  Plus, don't miss out on our Freestyle Skiing Workshop, where expert instructors will teach you the art of tricks and jumps. For those craving adventure, join us for our Backcountry Adventure Weekend, filled with guided tours, safety courses and many more exciting events.</p>
-        <a href="">Latest Events ></a>
+        <a class="white-button" href="#">Latest Events ></a>
     </div>
-</div>
+</section>
