605a5464e6d9e34f775d580d9461db9e7f9ef3c1
Trezzon <tcabillo1@nait.ca>
2024-03-14 13:26:57 -0600

    minor whitespace

--- page-about-us.php
+++ page-about-us.php
@@ -16,23 +16,25 @@ get_header();
 ?>
 <h2>About pacesetter</h2>
 <?php
-$args=array(
-                'post_type' => 'about_section',
-                'posts_per_page' => 8,
-                'post_status'=>'publish'
-            );
-            $loop=new WP_Query($args);
-            while ($loop->have_posts()){
-                $loop-> the_post();
-                ?>
-                <div>
-                    <img src="<?php echo esc_html( get_field("section_image") );?>"/>
-                <p><?php echo esc_html(get_field('about_us_text'));?></p>
-                </div>
+$args = array(
+    'post_type' => 'about_section',
+    'posts_per_page' => 8,
+    'post_status' => 'publish'
+);
+$loop = new WP_Query($args);
+while ($loop->have_posts()) {
+    $loop->the_post();
+    ?>
+    <div>
+        <img src="<?php echo esc_html(get_field("section_image")); ?>" />
+        <p>
+            <?php echo esc_html(get_field('about_us_text')); ?>
+        </p>
+    </div>
 
-                <?php
-    }
-        ?>
+    <?php
+}
+?>
 </section>
 <section>
     <h2>Our History</h2>
@@ -42,22 +44,26 @@ $args=array(
 <section>
     <h3>meet our staff!</h3>
     <?php
-            $args=array(
-                'post_type' => 'employee',
-                'posts_per_page' => 8,
-                'post_status'=>'publish'
-            );
-            $loop=new WP_Query($args);
-            while ($loop->have_posts()){
-                $loop-> the_post();
-                ?>
-                <div class="col">
-                    <img src="<?php echo esc_html( get_field("employee_image") );?>"/>
-                    <h4><?php echo esc_html(get_field('title'));?></h4>
-                <p><?php echo esc_html(get_field('employee_position'));?></p>
-                </div>
-
-                <?php
-    }
+    $args = array(
+        'post_type' => 'employee',
+        'posts_per_page' => 8,
+        'post_status' => 'publish'
+    );
+    $loop = new WP_Query($args);
+    while ($loop->have_posts()) {
+        $loop->the_post();
         ?>
+        <div class="col">
+            <img src="<?php echo esc_html(get_field("employee_image")); ?>" />
+            <h4>
+                <?php echo esc_html(get_field('title')); ?>
+            </h4>
+            <p>
+                <?php echo esc_html(get_field('employee_position')); ?>
+            </p>
+        </div>
+
+        <?php
+    }
+    ?>
 </section>
