26deba250ad1918fe06813a33816e74ed26ca6e9
Trezzon <tcabillo1@nait.ca>
2024-03-15 15:45:20 -0600

    Added styles javascript enqueue

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -5,12 +5,21 @@ function enqueue_styles() {
 }
 add_action('wp_enqueue_scripts', 'enqueue_styles');
 
+function enqueue_styles_scripts() {
+  // Enqueue Javascript for styling
+  wp_enqueue_script('styles-scripts', get_template_directory_uri() . '/js/styles-utils.js', array('jquery'), '1.0.0', true, true);
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_styles_scripts');
+
 function enqueue_custom_scripts() {
   // Enqueue jQuery with a specific version (you can adjust the version if needed)
   wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.6.4.min.js', array(), '3.6.4', true);
 
   // Enqueue your custom script with jQuery as a dependency
   wp_enqueue_script('events-form-submission-script', get_template_directory_uri() . '/js/events-modal.js', array('jquery'), null, true);
+
+  // Enqueue scripts for page styling
 
   // Pass data to the script
   wp_localize_script('events-form-submission-script', 'ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
