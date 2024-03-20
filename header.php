638e05cdfa990e223863894726d5dc795d6d1be3
Trezzon <tcabillo1@nait.ca>
2024-03-20 15:49:36 -0600

    info-nav styled and searchbar functionality added.

--- header.php
+++ header.php
@@ -41,47 +41,51 @@
 
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
-	<div id="page" class="site">
-		<div class="info-nav">
-			<div>
-				<p>
-					Mon-Fri: 9:30am - 5:30pm
-				</p>
-				<p>
-					(780) 483-2005
-				</p>
-			</div>
-			<div>
-				<a href="<?php echo wc_get_cart_url(); ?>">
-					<svg width="33px" height="33px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<title>This is an image of a cart</title>
-						<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-						<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-						<g id="SVGRepo_iconCarrier">
-							<path
-								d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
-								stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-						</g>
-					</svg>
-				</a>
-				<div id="main-search" class="hidden">
-					<?php get_search_form(); ?>
+	<header id="masthead" class="site-header">
+		<div id="page" class="site">
+			<div class="info-nav">
+				<div class="operation-hours">
+					<p>
+						Mon-Fri: 9:30am - 5:30pm
+					</p>
+					<p>
+						(780) 483-2005
+					</p>
 				</div>
-				<button type="button" id="show-search" class="show-search">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round"
-							d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-					</svg>
-				</button>
+				<div>
+					<a class="cart-link" href="<?php echo wc_get_cart_url(); ?>">
+						<svg width="33px" height="33px" viewBox="0 0 24 24" fill="none"
+							xmlns="http://www.w3.org/2000/svg">
+							<title>This is an image of a cart</title>
+							<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+							<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+							<g id="SVGRepo_iconCarrier">
+								<path
+									d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
+									stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+								</path>
+							</g>
+						</svg>
+					</a>
+					<div class="search-section">
+						<div id="main-search" class="hidden">
+							<?php get_search_form(); ?>
+						</div>
+						<button type="button" id="show-search" class="show-search">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+								stroke="currentColor">
+								<path stroke-linecap="round" stroke-linejoin="round"
+									d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+							</svg>
+						</button>
+					</div>
+				</div>
 			</div>
-		</div>
 
-		<a class="skip-link screen-reader-text" href="#primary">
-			<?php esc_html_e('Skip to content', 'pacesetter'); ?>
-		</a>
+			<a class="skip-link screen-reader-text" href="#primary">
+				<?php esc_html_e('Skip to content', 'pacesetter'); ?>
+			</a>
 
-		<header id="masthead" class="site-header">
 			<div class="site-branding">
 				<?php
 				the_custom_logo();
@@ -101,18 +105,20 @@
 				?>
 			</div><!-- .site-branding -->
 
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"
-					aria-label="Main Menu toggle">
-					<?php esc_html_e('Primary Menu', 'pacesetter'); ?>
-				</button>
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id' => 'primary-menu',
-					)
-				);
-				?>
-			</nav><!-- #site-navigation -->
-		</header><!-- #masthead -->
+			<div>
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"
+						aria-label="Main Menu toggle">
+						<?php esc_html_e('Primary Menu', 'pacesetter'); ?>
+					</button>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id' => 'primary-menu',
+						)
+					);
+					?>
+				</nav><!-- #site-navigation -->
+			</div>
+	</header><!-- #masthead -->
--- js/styles-utils.js
+++ js/styles-utils.js
@@ -1,7 +1,35 @@
-console.log("js connected");
+console.log("js connected"); // sanity check, remove in final
 
 let searchBtn = document.getElementById("show-search");
+let searchField = document.getElementById("main-search");
+let submitBtn = document.querySelector(".search-submit");
+let searchForm = document.querySelector(".search-form");
 
+// Search Field toggle for mobile
 searchBtn.addEventListener("click", () => {
-    document.getElementById("main-search").classList.toggle("hidden");
+    if (searchField.classList.contains("hidden")) {
+        document.querySelector("#main-search .search-field").placeholder = "Search Pacesetter..."
+        searchField.classList.toggle("hidden");
+
+        document.querySelector(".search-submit").classList.add("hidden");
+        document.querySelector(".search-section").classList.toggle("absolute-element");
+
+        document.querySelector(".cart-link").classList.toggle("hidden");
+    } else {
+        console.log("Search searched");
+
+    }
+});
+
+// toggles search bar closed if user clicks outside of it
+document.addEventListener('click', (evt) => {
+    const searchSection = document.querySelector('.search-section');
+    const clickedInside = searchSection.contains(evt.target);
+
+    if (!clickedInside && !searchField.classList.contains("hidden")) {
+        console.log("user clicked outside search");
+        searchField.classList.toggle("hidden");
+        document.querySelector(".search-section").classList.remove("absolute-element");
+        document.querySelector(".cart-link").classList.remove("hidden");
+    }
 });
