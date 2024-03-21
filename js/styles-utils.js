1c65439d6626baf7bd25f45148e977f73e54611b
Trezzon <tcabillo1@nait.ca>
2024-03-21 13:11:45 -0600

    main nav dropdown functional, started styling dropdown menu

--- js/styles-utils.js
+++ js/styles-utils.js
@@ -4,6 +4,7 @@ let searchBtn = document.getElementById("show-search");
 let searchField = document.getElementById("main-search");
 let submitBtn = document.querySelector(".search-submit");
 let searchForm = document.querySelector(".search-form");
+let mainNavToggle = document.querySelector(".main-nav-toggle");
 
 // Search Field toggle for mobile
 searchBtn.addEventListener("click", () => {
@@ -12,7 +13,7 @@ searchBtn.addEventListener("click", () => {
         searchField.classList.toggle("hidden");
 
         document.querySelector(".search-submit").classList.add("hidden");
-        document.querySelector(".search-section").classList.toggle("absolute-element");
+        document.querySelector(".search-section").classList.toggle("absolute");
 
         document.querySelector(".cart-link").classList.toggle("hidden");
     } else {
@@ -21,7 +22,7 @@ searchBtn.addEventListener("click", () => {
     }
 });
 
-// toggles search bar closed if user clicks outside of it
+// toggles search bar closed if user clicks outsidse of it
 document.addEventListener('click', (evt) => {
     const searchSection = document.querySelector('.search-section');
     const clickedInside = searchSection.contains(evt.target);
@@ -29,7 +30,18 @@ document.addEventListener('click', (evt) => {
     if (!clickedInside && !searchField.classList.contains("hidden")) {
         console.log("user clicked outside search");
         searchField.classList.toggle("hidden");
-        document.querySelector(".search-section").classList.remove("absolute-element");
+        document.querySelector(".search-section").classList.remove("absolute");
         document.querySelector(".cart-link").classList.remove("hidden");
     }
+});
+
+// toggles mega dropdown for mobile
+mainNavToggle.addEventListener('click', (evt) => {
+    console.log("Main Nav Toggle Clicked");
+    document.querySelector(".toggle-menu").classList.toggle("hidden");
+
+    document.querySelector(".main-nav-toggle .open").classList.toggle("hidden");
+    document.querySelector(".main-nav-toggle .close").classList.toggle("hidden");
+
+    document.querySelector("html").classList.toggle("overflow-hidden");
 });
