711784dd3efe35cfdfbbfd972f4967da0e854b4f
KirstynH <kirstynhowell01@gmail.com>
2024-03-28 23:40:36 -0600

    buttons and more

--- template-parts/featured-products.php
+++ template-parts/featured-products.php
@@ -1,4 +1,4 @@
-<section>
+<section class="container featured-products">
     <div>
             <h2>Featured Products</h2>
     <?php
@@ -38,5 +38,11 @@
         </div>
     
     <?php } ?>
-    <a class="yellow-btn" href="<?php echo esc_url($shop_page_url);?>">Shop All Products ></a>
+    <a class="yellow-btn btn" href="<?php echo esc_url($shop_page_url);?>">Shop All Products <svg fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	        viewBox="0 0 491.1 491.1" xml:space="preserve">
+            <g>
+                <path d="M379.25,282.85l-192.8,192.8c-20.6,20.6-54,20.6-74.6,0s-20.6-54,0-74.6l155.5-155.5l-155.5-155.5
+                    c-20.6-20.6-20.6-54,0-74.6s54-20.6,74.6,0l192.8,192.8C399.85,228.85,399.85,262.25,379.25,282.85z"/>
+            </g>
+            </svg></a>
 </section>
--- template-parts/hosted-events.php
+++ template-parts/hosted-events.php
@@ -1,11 +1,15 @@
 <section class="landing-page-events">
-    <div>
-        <img src="landingpage-events.jpg" alt="People having a good time at an event, seated around three people standing and talking.">
-    </div>
-    <div>
-        <h3>Store Hosted Events</h3>
+    <img src="https://pacesetter-b.web.dmitcapstone.ca/wp-content/themes/pacesetter-b/img/landingpage-events.jpg" alt="People having a good time at an event, seated around three people standing and talking.">
+    <div class="landing-page-events-text">
+        <h2>Store Hosted Events</h2>
         <p>Discover the latest events at our ski and snowboard store, guaranteed to ignite your winter excitement!  Plus, don't miss out on our Freestyle Skiing Workshop, where expert instructors will teach you the art of tricks and jumps. For those craving adventure, join us for our Backcountry Adventure Weekend, filled with guided tours, safety courses and many more exciting events.</p>
-        <a class="white-button" href="#">Latest Events ></a>
+        <a class="white-button btn" href="#">Latest Events <svg fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	        viewBox="0 0 491.1 491.1" xml:space="preserve">
+            <g>
+                <path d="M379.25,282.85l-192.8,192.8c-20.6,20.6-54,20.6-74.6,0s-20.6-54,0-74.6l155.5-155.5l-155.5-155.5
+                    c-20.6-20.6-20.6-54,0-74.6s54-20.6,74.6,0l192.8,192.8C399.85,228.85,399.85,262.25,379.25,282.85z"/>
+            </g>
+            </svg>
+        </a>
     </div>
-</section>
 </section>
