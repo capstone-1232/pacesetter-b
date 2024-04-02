f84f1f9f6573a5b0e0aa6809b2262076b389ef31
Terrence C. <118936370+Trezzon@users.noreply.github.com>
2024-04-01 21:05:35 -0600

    styled events basic static mobile view

--- page-events.php
+++ page-events.php
@@ -12,24 +12,35 @@ get_header();
 ?>
 
 <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+    <main id="main" class="site-main container" role="main">
         <section class="banner">
-            <h2>Events <span class="underline"></span></h2>
+            <h2>Events <span class="offset-underline"></span></h2>
             <p>All our events are free!</p>
         </section>
         <section>
             <div>
-                <h3>Filters</h3>
-                <div id="filters" class="filters">
-                    <p>Filters</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
-                    </svg>
+                <h3 class="sr-only">Filters</h3>
+                <div class="event-menu">
+                    <div class="filter-toggle">
+                        <p>Filters</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
+                        </svg>
+                    </div>
+                    <div>
+                        <p>Sort</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </div>
+                </div>
+                <div id="filters" class="event-filters">
                 </div>
                 <div class="sort">
-                    
+
                 </div>
             </div>
             <div id="removeFilterList"></div>
