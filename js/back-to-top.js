de5e7500085cab6063365ee2015b268f2b84b4bd
David Vo <davie.vo@live.com>
2024-04-07 18:56:26 -0600

    completed single products, updated footer

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -1,11 +1,36 @@
 (function () {
+  var backToTopBtn = document.querySelector("#backToTopBtn");
   document.addEventListener("DOMContentLoaded", function () {
     // Add an event listener to the back to top button
-    var backToTopBtn = document.querySelector("#backToTopBtn");
+    backToTopBtn.addEventListener("click", scrollToTop);
     function scrollToTop() {
       document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
     }
-    backToTopBtn.addEventListener("click", scrollToTop);
+
+    // Function to toggle button visibility based on scroll position
+    function toggleBackToTopBtn() {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        backToTopBtn.style.visibility = "visible";
+        backToTopBtn.style.opacity = "1";
+      } else {
+        backToTopBtn.style.visibility = "hidden";
+        backToTopBtn.style.opacity = "0";
+      }
+    }
+
+    // Event listener for scrolling
+    window.onscroll = function () {
+      toggleBackToTopBtn();
+    };
+
+    // Function to scroll to the top when the button is clicked
+    backToTopBtn.onclick = function () {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    };
   });
 })();
