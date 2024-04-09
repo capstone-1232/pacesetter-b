306db8e3ee9bc8a2be4e04d64783ee36721ed4b6
Trezzon <tcabillo1@nait.ca>
2024-04-08 20:47:15 -0600

    desktop products catalogue/taxonomy styled filters dropdown functionality

--- js/styles-utils.js
+++ js/styles-utils.js
@@ -7,7 +7,7 @@ $j(document).ready(function () {
 
     // on load color variables based on colors in css
     const colTurqoise = "#027F9A";
-    const colPrimaryBlue= "#013562";
+    const colPrimaryBlue = "#013562";
     const colStrongOrange = "#FC7803";
     const colBlack = "#000";
     const colWhite = "#fff";
@@ -142,9 +142,9 @@ $j(document).ready(function () {
     });
 
     if (window.matchMedia("(max-width: 1024px)").matches) {
-        $j(".rsvp-toggle").on("click", function() {
+        $j(".rsvp-toggle").on("click", function () {
             console.log("rsvp toggle clicked");
-    
+
             if ($j(".rsvp-panel").is(":visible")) {
                 $j(".rsvp-panel #myForm").submit();
             } else {
@@ -152,7 +152,7 @@ $j(document).ready(function () {
             }
         });
 
-        $j(".rsvp-panel div>button").on("click", function() {
+        $j(".rsvp-panel div>button").on("click", function () {
             $j(".rsvp-panel").hide("slide");
         });
     }
@@ -165,7 +165,7 @@ $j(document).ready(function () {
     });
 
     // Shows back to top button when user scrolls past a certain point
-    $j(window).scroll(function() {
+    $j(window).scroll(function () {
         var scrollPoint = 200; // replace with the point you're interested in
         if ($j(this).scrollTop() > scrollPoint) {
             $j('#backToTopBtn').show("fade");
@@ -175,50 +175,92 @@ $j(document).ready(function () {
     });
 
     // Scrolls back to top of page when back to top button is clicked
-    $j('#backToTopBtn').click(function() {
-        $j('html, body').animate({scrollTop: 0}, 'fast');
+    $j('#backToTopBtn').click(function () {
+        $j('html, body').animate({ scrollTop: 0 }, 'fast');
         return false;
     });
 
     // Toggles plus and minus icons on faq accordion headers
-    $j(".ui-accordion-header").on("click", function(evt){
+    $j(".faq .ui-accordion-header").on("click", function (evt) {
         $j(".ui-accordion-header .plus-icon").toggleClass("hidden");
         $j(".ui-accordion-header .minus-icon").toggleClass("hidden");
     });
 
     // Toggles products filter menu on mobile
-    $j(".show-filters, .product-filters div:first-of-type>button, .product-filters>button").on("click", function() {
-        $j(".product-filters").toggle("slide");
-    })
+    if (window.matchMedia("(max-width: 1024px)").matches) {
+        $j(".show-filters, .product-filters div:first-of-type>button, .product-filters>button").on("click", function () {
+            $j(".product-filters").toggle("slide");
+        })
+    }
 
     // Toggles style of filter option to show it is applied
-    $j(".ui-accordion-content>div").on("click", function() {
-        $j(this).parent().find(">div").css({
-            "background-color": colWhite,
-            "color": colBlack,
-            "border": "2px solid " + colStrongOrange,
+    if (window.matchMedia("(max-width: 1024px)").matches) {
+        $j(".ui-accordion-content>div").on("click", function () {
+            $j(this).parent().find(">div").css({
+                "background-color": colWhite,
+                "color": colBlack,
+                "border": "2px solid " + colStrongOrange,
+            });
+
+            $j(this).css({
+                "background-color": colTurqoise,
+                "color": "#fff",
+                "border": "2px solid " + colPrimaryBlue,
+            });
         });
-        
-        $j(this).css({
-            "background-color": colTurqoise,
-            "color": "#fff",
-            "border": "2px solid " + colPrimaryBlue,
-        });
-    });
+    }
 
     // Toggles style of filter items back to unchecked when filter is removed
-    $j(".removeFilterList").on("click", ".products-filter-remove", function() {
+    $j(".removeFilterList").on("click", ".products-filter-remove", function () {
         let filterText = $j(this).attr("data-value");
-        $j(".ui-accordion-content>div input").each(function() {
+        $j(".ui-accordion-content>div input").each(function () {
             if ($j(this).val() == filterText) {
-                $j(this).parent().css({
-                    "background-color": colWhite,
-                    "color": colBlack,
-                    "border": "2px solid " + colStrongOrange,
-                });
+
+                if (window.matchMedia("(max-width: 1024px)").matches) {
+                    $j(this).parent().css({
+                        "background-color": colWhite,
+                        "color": colBlack,
+                        "border": "2px solid " + colStrongOrange,
+                    });
+                }
+
+                $j(this).prop("checked", false).trigger("change");
             }
         });
     });
 
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+
+        // toggles other accordion dropdown not clicked
+        $j(".taxonomy-product .accordion").on("click", function () {
+            let activeAccordion = $j(this);
+            $j(".taxonomy-product .accordion").not(activeAccordion).each(function () {
+                $j(this).accordion("option", "active", false);
+            });
+        })
+
+        // toggles all accordions closed if user clicks outside of them
+        $j(document).on("click", function (evt) {
+            let targetElement = $j(".taxonomy-product .accordion");
+            if (!targetElement.is(evt.target) && targetElement.has(evt.target).length === 0) {
+                $j(".taxonomy-product .accordion").accordion("option", "active", false);
+            }
+        });
+
+        // toggles style of filter option to show it is applied
+        $j(".taxonomy-product .ui-accordion-content").on("change", function() {
+            let hasChecked = false;
+            $j(this).find("input").each(function() {
+                if ($j(this).prop("checked") == true) {
+                    hasChecked = true;
+                }
+            });
+            if (hasChecked) {
+                $j(this).parent().addClass("has-selected");
+            } else {
+                $j(this).parent().removeClass("has-selected");
+            }
+        });
+    }
 });
 
