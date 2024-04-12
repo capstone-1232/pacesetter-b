fd154535526bcaf7e059e86f433a93e1988eaf64
[neitlaus] <112661025+neitlaus@users.noreply.github.com>
2024-04-12 00:46:23 -0600

    text shadow

--- page-contact-us.php
+++ page-contact-us.php
@@ -1,12 +1,8 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying contact us pages
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
+ * 
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package pacesetter
@@ -15,12 +11,13 @@
 get_header();
 ?>
 <main class="contact">
-    <section class="contact-section">
+    <section class="contact-details">
         <h2>Contact us</h2>
         <span class="offset-underline"></span>
         <p>Give us a call, fill out our contact form, or come in person if you have any questions.
             We’re happy to help and have our expert staff available during hours of operation. </p>
-            <div>
+            
+            <div class="loop-flex">
 
         <?php $args = array(
             'post_type' => 'business',
@@ -49,32 +46,41 @@ get_header();
                 </p>
 
             </div>
-            <?php
-        }
-        ?>
+           
             </div>
         <p>Have questions about our store, gear, prices and anything in between?</p>
     </section>
-    <div>
-        <section>
-            <h2>Send us an email!</h2>
-            
-            <?php  echo do_shortcode('[forminator_form id="139"]'); ?>
+    <div class="section-flex">
+
+        <section class="email">
+            <div class="contact-form">
+                <h3>Send us an email!</h3>
+                <?php  echo do_shortcode('[forminator_form id="139"]'); ?>
+            </div>
+            <p class="or">or</p>
         </section>
-        <section>
-            <h2>FAQs</h2>
-            <p>Check out our FAQ page to find common questions</p>
-            
-            <a href="<?php echo home_url('/faq');?>"> FAQs</a>
-        </section>
+        <section class="faq-section">
+            <div>
+
+                <h3>FAQs</h3>
+                <p>Check out our FAQ page to find common questions</p>
+                
+                <a class="faq-button" href="<?php echo home_url('/faq');?>"> FAQs</a>
+            </div>
+            </section>
     </div>
-    <div>
+    <section class="map" style= "background: url(<?php echo home_url()?>/wp-content/themes/pacesetter-b/img/store-front-background.webp)">
+        <h2>Location</h2>
+        <p><?php echo get_field('address'); ?></p>
+        <p><?php echo get_field('city');?>, <?php echo get_field('province');?> <?php echo get_field('postal_code');?></p>
             <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d151740.59956275974!2d-113.8983764132812!3d53.54086290000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a02103230e0325%3A0xdb9361d3d0c6e002!2sPacesetter%20Ski%20Shoppe!5e0!3m2!1sen!2sca!4v1708037539783!5m2!1sen!2sca"
             width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
+    </section>
+</main>
     <?php
+        }
 get_footer()
     ?>
 
